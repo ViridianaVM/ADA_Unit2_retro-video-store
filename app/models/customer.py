cd126425787b3bdbc05d78900aa3a4c9f6360dfa
Rock - Viri Vazquez <viridiana.vzq@gmail.com>
2021-05-17 11:51:17 -0700

    Setup for project and Customer model created

---
+++ app/models/customer.py
@@ -0,0 +1,16 @@
+from flask import current_app
+from app import db
+
+class Customer(db.Model):
+    customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.Integer)
+    phone = db.Column(db.String)
+
+    def to_json(self):
+        return{
+            "id": self.customer_id,
+            "name": self.name,
+            "postal_code": self.postal_code,
+            "phone": self.phone
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,11 @@
-from flask import Blueprint
+from app import db
+import requests
+from flask import request, Blueprint, make_response
+from flask import jsonify
+from .models.customer import Customer
+from datetime import datetime
+from dotenv import load_dotenv
+import os
 
+
+customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
