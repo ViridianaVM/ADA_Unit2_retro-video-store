5b7c14b63fd79908d9f766fd90d51aa323902526
Rock - Viri Vazquez <viridiana.vzq@gmail.com>
2021-05-18 23:51:06 -0700

    Added available_inventory attribute for video and changed postal_code attribute as Int in customer

--- app/models/customer.py
+++ app/models/customer.py
@@ -6,17 +6,17 @@ class Customer(db.Model):
     __tablename__ = "customers"
     customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    postal_code = db.Column(db.String)
+    postal_code = db.Column(db.Integer)
     phone = db.Column(db.String)
-    videos_checked_out_count = db.Column(db.Integer)
+    videos_checked_out_count = db.Column(db.Integer, default=0)
     registered_at = db.Column(db.DateTime, nullable=True)
     
-    def videos_checked(self):
-        if self.videos_checked_out_count is None:
-            videos_checked = 0
-        else:
-            videos_checked = self.videos_checked_out_count
-        return videos_checked
+    # def videos_checked(self):
+    #     if self.videos_checked_out_count is None:
+    #         videos_checked = 0
+    #     else:
+    #         videos_checked = self.videos_checked_out_count
+    #     return videos_checked
 
     def to_json(self):
         return {
@@ -24,6 +24,6 @@ class Customer(db.Model):
             "name": self.name,
             "postal_code": self.postal_code,
             "phone": self.phone,
-            "videos_checked_out_count": self.videos_checked(),
+            "videos_checked_out_count": self.videos_checked_out_count,
             "registered_at": self.registered_at
         }
--- app/models/video.py
+++ app/models/video.py
@@ -8,6 +8,7 @@ class Video(db.Model):
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime, nullable=True)
     total_inventory = db.Column(db.Integer)
+    available_inventory = db.Column(db.Integer)
     
     
 
@@ -16,5 +17,6 @@ class Video(db.Model):
             "id": self.video_id,
             "title": self.title,
             "release_date": self.release_date,
-            "total_inventory": self.total_inventory
+            "total_inventory": self.total_inventory,
+            "available_inventory": self.available_inventory
         }
